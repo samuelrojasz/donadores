228f7472a2b7f242cf0aa363498530e1adde78f2
clarosgerd <carlitos.gerd@gmail.com>
2017-07-19 10:52:57 -0400

    otro cambio

--- ewcfg12.php
+++ ewcfg12.php
@@ -29,7 +29,7 @@ define("EW_CONFIG_FILE_FOLDER", EW_PROJECT_NAME . "", TRUE); // Config file name
 define("EW_PROJECT_ID", "{A9B917F6-72DB-4C37-BB0D-F508A0EFFBF8}", TRUE); // Project ID (GUID)
 $EW_RELATED_PROJECT_ID = "";
 $EW_RELATED_LANGUAGE_FOLDER = "";
-define("EW_RANDOM_KEY", '02RKAx2yyg53xwvP', TRUE); // Random key for encryption
+define("EW_RANDOM_KEY", '678C9paj359f03gV', TRUE); // Random key for encryption
 define("EW_PROJECT_STYLESHEET_FILENAME", "phpcss/donadores.css", TRUE); // Project stylesheet file name
 define("EW_CHARSET", "utf-8", TRUE); // Project charset
 define("EW_EMAIL_CHARSET", EW_CHARSET, TRUE); // Email charset
